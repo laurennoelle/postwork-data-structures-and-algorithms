377a54291feb938f8d87fa3544546d67437aa5ea
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-09-22 16:24:16 -0600

    reversed string done

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,28 +1,20 @@
-// create new variable that contains result
-// iterate through the length of the original string minus 1, to start with the last character and go backwards until the first character is reached
-// store each character back into our new variable, with every single index that we encounter
-// result stored in the new variable that gets returned
 
 function reverseString(str) {
   let newStr = "";
   for (let i = str.length-1; i >= 0; i--) {
     newStr += str[i];
   }
-   return newStr
+  return newStr
 }
 
-// Second Solution: 
-// function reverseString(str) {
-//    return str.split("").reverse().join("")
-//}
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
-
+  
   console.log("");
-
+  
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
 }
@@ -30,4 +22,24 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+/*************************************** 
+ * create new variable that contains result
+ * 
+ * iterate through the length of the original string minus 1, to start with the last character and go backwards until the first character is reached
+ *
+ * store each character back into our new variable, with every single index that we encounter
+ * 
+ * result stored in the new variable that gets returned
+ * 
+ *******************************************/
+
+
+
+//   And a written explanation of your solution
+/**********************************************
+Second Solution: 
+function reverseString(str) {
+  return str.split("").reverse().join("")
+}
+ 
+ * ********************************************/
